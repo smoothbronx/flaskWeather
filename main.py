e08b1715ab1fe11a82635d5df64f30a6d75ea35b
bronx <yxbatobv@mail.ru>
2020-04-27 19:02:28 +0200

    Day 2. Added user authorization. Each user can now choose cities individually for themselves. in the next few days I'm going to adjust the design and edit the code

--- main.py
+++ main.py
@@ -1,60 +1,71 @@
 import requests
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, redirect, url_for, request, flash
+from flask_login import login_user, login_required, logout_user, UserMixin, LoginManager, current_user
+from werkzeug.security import check_password_hash, generate_password_hash
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///weather.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.config['SECRET_KEY'] = 'bronxSN'
-
 db = SQLAlchemy(app)
+manager = LoginManager(app)
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    login = db.Column(db.String(128), nullable=False, unique=True)
+    password = db.Column(db.String(255), nullable=False)
+
+
+@manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
 
 class City(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
+    userid = db.Column(nullable=False)
+
 
 def get_weather_data(city):
     url = f'http://api.openweathermap.org/data/2.5/weather?q={ city }&units=metric&appid=271d1234d3f497eed5b1d80a07b3fcd1'
     r = requests.get(url).json()
     return r
 
+
+
 @app.route('/')
+@login_required
 def index_get():
-    cities = City.query.all()
-
+    cities = City.query.filter_by(userid=current_user.id)
     weather_data = []
-
     for city in cities:
-
         r = get_weather_data(city.name)
-        print(r)
+        print(current_user.id)
 
         weather = {
-            'city' : city.name,
-            'temperature' : r['main']['temp'],
-            'description' : r['weather'][0]['description'],
-            'icon' : r['weather'][0]['icon'],
+            'city': city.name,
+            'temperature': r['main']['temp'],
+            'description': r['weather'][0]['description'],
+            'icon': r['weather'][0]['icon'],
         }
-
         weather_data.append(weather)
-
-
     return render_template('index.html', weather_data=weather_data)
+
 
 @app.route('/', methods=['POST'])
 def index_post():
     err_msg = ''
     new_city = request.form.get('city')
-
     if new_city:
-        existing_city = City.query.filter_by(name=new_city).first()
-
+        existing_city = City.query.filter_by(userid=current_user.id, name=new_city).first()
         if not existing_city:
             new_city_data = get_weather_data(new_city)
-
             if new_city_data['cod'] == 200:
-                new_city_obj = City(name=new_city)
+                new_city_obj = City(name=new_city, userid=current_user.id)
 
                 db.session.add(new_city_obj)
                 db.session.commit()
@@ -67,17 +78,70 @@ def index_post():
         flash(err_msg, 'error')
     else:
         flash('City added succesfully!')
-
     return redirect(url_for('index_get'))
+
 
 @app.route('/delete/<name>')
 def delete_city(name):
-    city = City.query.filter_by(name=name).first()
+    city = City.query.filter_by(userid=current_user.id, name=name).first()
     db.session.delete(city)
     db.session.commit()
 
     flash(f'Successfully deleted { city.name }', 'success')
     return redirect(url_for('index_get'))
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login_page():
+    login = request.form.get('login')
+    password = request.form.get('password')
+    if login and password:
+        user = User.query.filter_by(login=login).first()
+        if user and check_password_hash(user.password, password):
+            login_user(user)
+            return redirect('/')
+        else:
+            flash('Login or password is not correct')
+    else:
+        flash('Please fill login and password fields')
+    return render_template('login.html')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    login = request.form.get('login')
+    password = request.form.get('password')
+    password2 = request.form.get('password2')
+
+    if request.method == 'POST':
+        if not (login or password or password2):
+            flash('Please, fill all fields!')
+        elif password != password2:
+            flash('Passwords are not equal!')
+        else:
+            hash_pwd = generate_password_hash(password)
+            new_user = User(login=login, password=hash_pwd)
+            db.session.add(new_user)
+            db.session.commit()
+
+            return redirect(url_for('login_page'))
+
+    return render_template('register.html')
+
+
+@app.route('/logout', methods=['GET', 'POST'])
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login_page'))
+
+
+@app.after_request
+def redirect_to_signin(response):
+    if response.status_code == 401:
+        return redirect(url_for('login_page') + '?next=' + request.url)
+    return response
+
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
